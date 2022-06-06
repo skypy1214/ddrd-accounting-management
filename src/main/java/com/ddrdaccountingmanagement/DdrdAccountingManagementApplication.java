a0e21b95f2260b2fa905e0ab690bde2bef193f25
dlehdgus6251 <dlehdgus6251.naver.com>
2022-06-06 23:56:45 +0900

    Initial commit

---
+++ src/main/java/com/ddrdaccountingmanagement/DdrdAccountingManagementApplication.java
@@ -0,0 +1,14 @@
+package com.ddrdaccountingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class DdrdAccountingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdrdAccountingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddrdaccountingmanagement/DdrdAccountingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddrdaccountingmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdrdAccountingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
